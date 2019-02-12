4010eb368f0de5c557ea78069c03c47034c29af1
Samiha Rahman <hellosamiha@gmail.com>
2019-02-12 18:45:30 -0500

    Removing admin navigation bar, fixes #19

--- woofer-theme/inc/template-functions.php
+++ woofer-theme/inc/template-functions.php
@@ -80,3 +80,12 @@ add_role('woofer', 'Woofer', array(
 ));
 
 
+// Remove admin bar
+add_action('after_setup_theme', 'remove_admin_bar');
+
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
+
