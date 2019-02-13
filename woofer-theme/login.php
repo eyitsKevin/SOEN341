2ec3e4ca5114f3301a9c03d80425322791c9b604
Weigiu <michael842055@hotmail.com>
2019-02-13 00:54:43 -0500

    user profile template

---
+++ woofer-theme/login.php
@@ -0,0 +1,36 @@
+"<?php
+/*
+*
+* Template Name: profile
+*
+*
+*/
+get_header();
+?>
+<?php if(is_user_logged_in()):?>
+  <h1 style=""margin:50px;"">
+    Post a woof
+  </h1>
+  <form method=""POST"" style=padding:50px;>
+      <label>Woof :</label>
+      <textarea value="""" rows=""3"" class=""input-xlarge"" name='text'>
+
+      </textarea>
+
+      <div>
+        <button class=""btn btn-primary"">Woof!</button>
+      </div>
+      <input type=""hidden"" name=""action"" value=""woof"" />
+   </from>
+<?php else: ?>
+  <h1 style=""margin:50px;"">
+    You need to login !!
+  </h1>
+
+<?php endif;?>
+
+
+<?php
+get_footer();
+?>
+"
