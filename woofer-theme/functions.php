6ba6b58a21245cce340231a79dc99c615c18cc7d
Thierry R. Le Bourdais <thierryr@orphic.ca>
2019-02-13 23:49:39 -0500

    fixed #57
    Linked AJAX called to WP ajax file and created ajax-url variable in follow.js

--- woofer-theme/functions.php
+++ woofer-theme/functions.php
@@ -159,3 +159,17 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+
+////////////////////////////////////
+// enqueue and reference AJAX calls
+////////////////////////////////////
+
+function my_enqueue() {
+
+    wp_enqueue_script( 'ajax-script', get_template_directory_uri() . '/js/follow.js', array('jquery') );
+
+    wp_localize_script( 'ajax-script', 'my_ajax_object',
+            array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+
+}
+add_action( 'wp_enqueue_scripts', 'my_enqueue' );
