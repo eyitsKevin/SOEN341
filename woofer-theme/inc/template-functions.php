c9d2267e3878e69c8d719cc42ee8293090565fd7
Victor Tudoran <33841928+VictorTudoran@users.noreply.github.com>
2019-02-13 19:17:13 -0500

    Create custom login form
    This piece of code handles the displaying of the form and the handling of the data. On a successful login, the user is redirected to the homepage. On a unsuccessful login, the form simply wipes the information and the user needs to start over.

--- woofer-theme/inc/template-functions.php
+++ woofer-theme/inc/template-functions.php
@@ -148,3 +148,59 @@ if ( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POS
 /////////////////////////////////////////////////////////
 
 require get_template_directory() . '/inc/woof-post-type.php';
+
+
+
+/////////////////////////////////////////////////////////
+// Woof custom login form
+/////////////////////////////////////////////////////////
+
+
+function woof_custom_login( $args = array() ) {
+    $defaults = array(
+        'echo' => true,
+        'redirect' => 'https://orphic.ca/soen331/',
+        'form_id' => 'loginform',
+        'label_username' => __( 'Username or Email Address' ),
+        'label_password' => __( 'Password' ),
+        'label_remember' => __( 'Remember Me' ),
+        'label_log_in' => __( 'Log In' ),
+        'id_username' => 'user_login',
+        'id_password' => 'user_pass',
+        'id_remember' => 'rememberme',
+        'id_submit' => 'wp-submit',
+        'remember' => true,
+        'value_username' => '',
+        'value_remember' => false,
+    );
+    $args = wp_parse_args( $args, apply_filters( 'login_form_defaults', $defaults ) );
+    $login_form_top = apply_filters( 'login_form_top', '', $args );
+    $login_form_middle = apply_filters( 'login_form_middle', '', $args );
+    $login_form_bottom = apply_filters( 'login_form_bottom', '', $args );
+
+    $form = '
+        <form name="' . $args['form_id'] . '" id="' . $args['form_id'] . '" action="' . esc_url( site_url( 'wp-login.php', 'login_post' ) ) . '" method="post">
+            ' . $login_form_top . '
+            <p class="login-username">
+                <label for="' . esc_attr( $args['id_username'] ) . '">' . esc_html( $args['label_username'] ) . '</label>
+                <input type="text" name="log" id="' . esc_attr( $args['id_username'] ) . '" class="input" value="' . esc_attr( $args['value_username'] ) . '" size="20" />
+            </p>
+            <p class="login-password">
+                <label for="' . esc_attr( $args['id_password'] ) . '">' . esc_html( $args['label_password'] ) . '</label>
+                <input type="password" name="pwd" id="' . esc_attr( $args['id_password'] ) . '" class="input" value="" size="20" />
+            </p>
+            ' . $login_form_middle . '
+            ' . ( $args['remember'] ? '<p class="login-remember"><label><input name="rememberme" type="checkbox" id="' . esc_attr( $args['id_remember'] ) . '" value="forever"' . ( $args['value_remember'] ? ' checked="checked"' : '' ) . ' /> ' . esc_html( $args['label_remember'] ) . '</label></p>' : '' )
+						 . '
+            <p class="login-submit">
+                <input type="submit" name="wp-submit" id="' . esc_attr( $args['id_submit'] ) . '" class="button button-primary" value="' . esc_attr( $args['label_log_in'] ) . '" />
+                <input type="hidden" name="redirect_to" value="' . esc_url( $args['redirect'] ) . '" />
+            </p>
+            ' . $login_form_bottom . '
+        </form>';
+
+    if ( $args['echo'] )
+        echo $form;
+    else
+        return $form;
+}
