d64faf00321e4a613f7b3b56bcd378c933a0ae00
Donya <23021061+meshgin@users.noreply.github.com>
2019-02-13 19:30:54 -0500

    Added page template for user login

---
+++ woofer-theme/login.php
@@ -0,0 +1,20 @@
+<?php
+/*
+*
+* Template Name: login
+*
+*
+*/
+get_header();
+?>
+
+<h1 style="margin:100px;">
+  Login
+</h1>
+
+<div style="margin:50px;">
+<?php
+ woof_custom_login();
+
+?>
+</div>
