1b71fb87afa3783e83e2135f48c38bb0bd4a217c
Thierry R. Le Bourdais <thierry.lebourdais@gmail.com>
2019-02-13 23:01:44 -0500

    Fixed the double quotes typos

--- woofer-theme/template-parts/show-woofs-on-profile.php
+++ woofer-theme/template-parts/show-woofs-on-profile.php
@@ -7,10 +7,10 @@ $currID = $current_user->ID;
     $loop = new WP_Query( array( 'post_type' => 'woof','author'=> $current_user->ID) );
     if ( $loop->have_posts() ) :
         while ( $loop->have_posts() ) : $loop->the_post(); ?>
-        <div style=""border:1px solid black;margin:75px;"">
-            <div style=""margin:20px;font-weight:600;color:red;display:inline-block;vertical-align:top;"">
+        <div style="border:1px solid black;margin:75px;">
+            <div style="margin:20px;font-weight:600;color:red;display:inline-block;vertical-align:top;">
             <?php echo the_author_nickname(); ?>
-            <div style=""color:black;font-weight:400;"">
+            <div style="color:black;font-weight:400;">
               <?php echo the_content(); ?>
             </div>
           </div>
